a08f6bf142ce3643a7922677c39ae9cacadfbceb
临渊 <1471166577@qq.com>
2021-06-29 08:34:03 +0800

    分布式事务

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
@@ -1,6 +1,20 @@
 package com.atguigu.gmall.pms.service.impl;
 
+import com.atguigu.gmall.pms.entity.*;
+import com.atguigu.gmall.pms.feign.GmallSmsClient;
+import com.atguigu.gmall.pms.mapper.SkuMapper;
+import com.atguigu.gmall.pms.mapper.SpuDescMapper;
+import com.atguigu.gmall.pms.service.SkuAttrValueService;
+import com.atguigu.gmall.pms.service.SkuImagesService;
+import com.atguigu.gmall.pms.service.SpuAttrValueService;
+import com.atguigu.gmall.pms.vo.SkuVo;
+import com.atguigu.gmall.pms.vo.SpuAttrValueVo;
+import com.atguigu.gmall.pms.vo.SpuVo;
+import com.atguigu.gmall.sms.vo.SkuSaleVo;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -9,12 +23,32 @@ import com.atguigu.gmall.common.bean.PageResultVo;
 import com.atguigu.gmall.common.bean.PageParamVo;
 
 import com.atguigu.gmall.pms.mapper.SpuMapper;
-import com.atguigu.gmall.pms.entity.SpuEntity;
 import com.atguigu.gmall.pms.service.SpuService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service("spuService")
 public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements SpuService {
+
+    @Autowired
+    private SpuDescMapper descMapper;
+
+    @Autowired
+    private SpuAttrValueService baseService;
+
+    @Autowired
+    private SkuMapper skuMapper;
+    @Autowired
+    private SkuImagesService skuImagesService;
+    @Autowired
+    private SkuAttrValueService skuAttrValueService;
+    @Autowired
+    private GmallSmsClient smsClient;
 
     @Override
     public PageResultVo queryPage(PageParamVo paramVo) {
@@ -39,6 +73,97 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements
         }
 
         return new PageResultVo(this.page(paramVo.getPage(),queryWrapper));
+    }
+
+    @GlobalTransactional
+    @Override
+    public void bigSave(SpuVo spuVo) {
+        /// 1.保存spu相关
+        // 1.1. 保存spu基本信息 spu_info
+        Long spuId = saveSpu(spuVo);
+
+        // 1.2. 保存spu的描述信息 spu_info_desc
+        saveSpuDesc(spuVo, spuId);
+
+        // 1.3. 保存spu的规格参数信息
+        saveBaseAttr(spuVo, spuId);
+
+        /// 2. 保存sku相关信息
+        saveSku(spuVo, spuId);
+
+
+    }
+
+    private void saveSku(SpuVo spuVo, Long spuId) {
+        List<SkuVo> skus = spuVo.getSkus();
+        if (CollectionUtils.isEmpty(skus)){
+            return;
+        }
+        skus.forEach(skuVo -> {
+            skuVo.setSpuId(spuId);
+            skuVo.setBrandId(spuVo.getBrandId());
+            skuVo.setCategoryId(spuVo.getCategoryId());
+
+            List<String> images = skuVo.getImages();
+            if (!CollectionUtils.isEmpty(images)){
+                skuVo.setDefaultImage(StringUtils.isBlank(skuVo.getDefaultImage()) ? images.get(0) : skuVo.getDefaultImage());
+            }
+            this.skuMapper.insert(skuVo);
+            Long skuId = skuVo.getId();
+
+            if (!CollectionUtils.isEmpty(images)){
+                this.skuImagesService.saveBatch(images.stream().map(image ->{
+                    SkuImagesEntity skuImagesEntity = new SkuImagesEntity();
+                    skuImagesEntity.setSkuId(skuId);
+                    skuImagesEntity.setUrl(image);
+                    skuImagesEntity.setDefaultStatus(StringUtils.equals(skuVo.getDefaultImage(),image) ? 1 : 0);
+                    return skuImagesEntity;
+                }).collect(Collectors.toList()));
+            }
+
+            List<SkuAttrValueEntity> saleAttrs = skuVo.getSaleAttrs();
+            if (!CollectionUtils.isEmpty(saleAttrs)){
+                saleAttrs.forEach(skuAttrValueEntity -> {
+                    skuAttrValueEntity.setSkuId(skuId);
+                });
+                this.skuAttrValueService.saveBatch(saleAttrs);
+            }
+            SkuSaleVo skuSaleVo = new SkuSaleVo();
+            BeanUtils.copyProperties(skuVo,skuSaleVo);
+            skuSaleVo.setSkuId(skuId);
+            this.smsClient.saveSales(skuSaleVo);
+        });
+    }
+
+    private void saveBaseAttr(SpuVo spuVo, Long spuId) {
+        List<SpuAttrValueVo> baseAttrs = spuVo.getBaseAttrs();
+        if (!CollectionUtils.isEmpty(baseAttrs)){
+            List<SpuAttrValueEntity> spuAttrValueEntities = baseAttrs.stream().map(spuAttrValueVo -> {
+                spuAttrValueVo.setSpuId(spuId);
+                spuAttrValueVo.setSort(0);
+                return spuAttrValueVo;
+            }).collect(Collectors.toList());
+            this.baseService.saveBatch(spuAttrValueEntities);
+        }
+    }
+
+    private void saveSpuDesc(SpuVo spuVo, Long spuId) {
+        List<String> spuImages = spuVo.getSpuImages();
+        if (!CollectionUtils.isEmpty(spuImages)){
+
+            SpuDescEntity spuDescEntity = new SpuDescEntity();
+            spuDescEntity.setSpuId(spuId);
+            spuDescEntity.setDecript(StringUtils.join(spuImages,","));
+            this.descMapper.insert(spuDescEntity);
+        }
+    }
+
+    private Long saveSpu(SpuVo spuVo) {
+        spuVo.setPublishStatus(1);
+        spuVo.setCreateTime(new Date());
+        spuVo.setUpdateTime(spuVo.getCreateTime());
+        this.save(spuVo);
+        return spuVo.getId();
     }
 
 }
--- gmall-sms/src/main/java/com/atguigu/gmall/sms/service/impl/SkuBoundsServiceImpl.java
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/service/impl/SkuBoundsServiceImpl.java
@@ -1,6 +1,15 @@
 package com.atguigu.gmall.sms.service.impl;
 
+import com.atguigu.gmall.sms.entity.SkuFullReductionEntity;
+import com.atguigu.gmall.sms.entity.SkuLadderEntity;
+import com.atguigu.gmall.sms.mapper.SkuFullReductionMapper;
+import com.atguigu.gmall.sms.mapper.SkuLadderMapper;
+import com.atguigu.gmall.sms.vo.SkuSaleVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,10 +20,18 @@ import com.atguigu.gmall.common.bean.PageParamVo;
 import com.atguigu.gmall.sms.mapper.SkuBoundsMapper;
 import com.atguigu.gmall.sms.entity.SkuBoundsEntity;
 import com.atguigu.gmall.sms.service.SkuBoundsService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 
 @Service("skuBoundsService")
 public class SkuBoundsServiceImpl extends ServiceImpl<SkuBoundsMapper, SkuBoundsEntity> implements SkuBoundsService {
+
+    @Autowired
+    private SkuFullReductionMapper skuFullReductionMapper;
+
+    @Autowired
+    private SkuLadderMapper skuLadderMapper;
 
     @Override
     public PageResultVo queryPage(PageParamVo paramVo) {
@@ -24,6 +41,29 @@ public class SkuBoundsServiceImpl extends ServiceImpl<SkuBoundsMapper, SkuBounds
         );
 
         return new PageResultVo(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveSales(SkuSaleVo skuSaleVo) {
+        SkuBoundsEntity skuBoundsEntity = new SkuBoundsEntity();
+        BeanUtils.copyProperties(skuSaleVo,skuBoundsEntity);
+        List<Integer> work = skuSaleVo.getWork();
+        if (!CollectionUtils.isEmpty(work) || work.size() == 4){
+            skuBoundsEntity.setWork(work.get(3) * 8 + work.get(2) * 4 + work.get(1) * 2 + work.get(0));
+
+        }
+        this.save(skuBoundsEntity);
+
+        SkuFullReductionEntity skuFullReductionEntity = new SkuFullReductionEntity();
+        BeanUtils.copyProperties(skuSaleVo,skuFullReductionEntity);
+        skuFullReductionEntity.setAddOther(skuSaleVo.getFullAddOther());
+        this.skuFullReductionMapper.insert(skuFullReductionEntity);
+
+        SkuLadderEntity skuLadderEntity = new SkuLadderEntity();
+        BeanUtils.copyProperties(skuSaleVo,skuLadderEntity);
+        skuLadderEntity.setAddOther(skuSaleVo.getFullAddOther());
+        this.skuLadderMapper.insert(skuLadderEntity);
     }
 
 }
